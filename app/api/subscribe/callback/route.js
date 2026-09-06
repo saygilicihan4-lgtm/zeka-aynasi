9036a883b63b5b476f8ec29af11e4b132824e4ab
saygilicihan4-lgtm <saygilicihan4@gmail.com>
2026-09-06 19:14:31 +0300

    Update route.js to export dynamic with force-dynamic

--- app/api/subscribe/callback/route.js
+++ app/api/subscribe/callback/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import Iyzipay from "iyzipay";
-
+export const dynamic - force-dynamic
 // iyzico, kullanıcı Ödeme Formu'nu tamamladıktan sonra tarayıcıyı bu adrese
 // bir "token" ile birlikte POST eder. Burada ödemenin gerçekten başarılı olup
 // olmadığı iyzico sunucusundan tekrar sorgulanarak doğrulanır (tek başına
